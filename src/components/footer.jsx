1fcb0a5eaa320c0bbd7019c458912b1f22875ebe
ShtefanM <melnikstepan.a@yandex.ru>
2024-05-29 16:16:11 +0300

    add files to GitHum

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return <footer className="page-footer black accent-3">
+        <div className="footer-copyright">
+            <div className="container">
+            © {new Date().getFullYear()} @Kitttydi
+            <a className="grey-text text-lighten-4 right" href="#!">Repository</a>
+            </div>
+          </div>
+    </footer>
+}
+export {Footer};
---
+++ src/components/header.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+    return <nav className=" black accent-3">
+        <div className="container">
+            <div className="nav-wrapper" >
+                <Link to="/" href="#!" class="brand-logo">
+                Recipes
+                </Link>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
+    </nav>
+}
+
+export { Header };
